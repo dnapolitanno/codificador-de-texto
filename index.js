d67825b3a7e8681080b0ff86b936a1f6ee5317a0
dnapolitanno <dudadnapolitano@gmail.com>
2024-08-27 18:02:34 -0300

    add codificador

---
+++ index.js
@@ -0,0 +1,72 @@
+var textoEntrada = document.getElementById("textoEntrada");
+var textoResultado = document.getElementById("textoResultado");
+var btnCriptografar = document.getElementById("btnCripto");
+var btnDescriptografar = document.getElementById("btnDescripto");
+var btnCopiar = document.getElementById("btnCopiar");
+var containerMessage = document.getElementById("container-sem-resultado");
+var containerResultado = document.getElementById("container-resultado");
+
+function criptografarLetras(frase) {
+  frase = frase
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat");
+  return frase;
+}
+
+function decriptografarLetras(frase) {
+  frase = frase
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
+  return frase;
+}
+
+btnCriptografar.addEventListener("click", function (event) {
+  if (textoEntrada.value) {
+    textoEntrada.value = criptografarLetras(textoEntrada.value);
+    console.log(textoEntrada.value);
+    textoResultado.value = textoEntrada.value;
+
+    textoResultado.style.height = "auto";
+    textoResultado.style.height = textoResultado.scrollHeight + "px";
+
+    textoEntrada.value = "";
+
+    containerMessage.style.display = "none";
+    containerResultado.style.display = "flex";
+  }
+});
+
+textoResultado.addEventListener("input", function () {
+  this.style.height = "auto";
+  this.style.height = this.scrollHeight + "px";
+});
+
+btnDescriptografar.addEventListener("click", function (event) {
+  if (textoEntrada.value) {
+    textoEntrada.value = decriptografarLetras(textoEntrada.value);
+    console.log(textoEntrada.value);
+    textoResultado.value = textoEntrada.value;
+    textoEntrada.value = "";
+
+    textoResultado.style.height = "auto";
+    textoResultado.style.height = textoResultado.scrollHeight + "px";
+
+    containerMessage.style.display = "none";
+    containerResultado.style.display = "flex";
+  }
+});
+
+btnCopiar.addEventListener("click", function (event) {
+  if (textoResultado.value) {
+    textoResultado.select();
+    textoResultado.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Texto copiado com sucesso: " + textoResultado.value);
+  }
+});
